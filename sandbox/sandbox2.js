04b41e50d04013aee73af8bd992fe6be1ef18d8f
antoine zimmermann <antoine.zimmermann@storetail.com>
2015-08-11 15:49:57 -0500

    maj

--- sandbox/sandbox2.js
+++ sandbox/sandbox2.js
@@ -1,6 +1,13 @@
 "use strict";
 
-var Promise = require( "../lib/promise.js" );
+var Promise = require( "../lib/promisePrototype.js" );
 
 
 console.log( typeof Promise );
+
+var p1 = new Promise(),
+    p2 = p1.then( function ( x ) {
+        console.log( "p1 is a success with " + x );
+    } );
+
+p1.resolve( "x" )
---
+++ sandbox/test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var Promise = require("../lib/promise.js");
+
+var p1 = new Promise();
+
+p1.then();
