04a25f96f2c240c3fc4724fe9cb1a0771ee94996
antoine zimmermann <antoine.zimmermann@storetail.com>
2015-08-11 21:55:31 -0500

    p

--- sandbox/test.js
+++ sandbox/test.js
@@ -1,12 +1,6 @@
-"use strict";
+(function (promise) {
+    "use strict";
+    var promise = promise.deferred();
 
-var promise = window.promise;
 
-var p1 = promise.deferred().promise,
-    p2 = p1.then( function ( val ) {
-        console.log( "ma valuer est dans le browser " + val );
-    } );
-
-setTimeout(function( ) {
-    p1.resolve( "YOLO" );
-}, 1000);
+} (promise));
