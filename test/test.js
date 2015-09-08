55332789a07c14c3eb83e4989d8a24c0ff64202c
antoine.zimmermann@storetail.com <antoine.zimmermann@storetail.com>
2015-09-08 10:45:40 +0200

    majs ./test/test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,25 @@
-var promise = require("./adapter.js");
+var promise = require( "./adapter.js" );
 
 
-var rejected = promise.rejected("dummy");
+var rejected = promise.rejected( "dummy" );
 
-rejected.then(function () {}, undefined).then(function () { console.log("should not be here"); }, function () {console.log("done")});
+rejected.then( function () {
+}, undefined ).then( function () {
+    console.log( "should not be here" );
+}, function ( reason ) {
+    console.log( reason )
+} );
+
+
+var resolved = promise.resolved( "resolved" ).then( function () {
+    return {
+        "then": function ( a, b ) {
+            a( "une autre valeur" );
+            setTimeout( function () {
+                throw Error( "une erreur" );
+            }, 0 );
+        }
+    };
+} ).then( function ( v ) {
+    console.log( v );
+} );
