60a52c79eee8f43f379c2bd31daf594facd93dda
antoine zimmermann <antoine.zimmermann@storetail.com>
2015-10-10 15:13:36 +0200

    add gist to wrap the ajax function of jquery

---
+++ wrapAjax.js
@@ -0,0 +1,30 @@
+"use strict";
+var promise = require("promise");//or any other promise library
+
+var wrappedAjax
+function wrapAjax($) {
+    // $ : jQuery
+    
+    if (wrappedAjax) return wrappedAjax
+    
+    wrappedAjax = function (o) {
+        
+        if (typeof o !== "object") throw new TypeError("Invalid argument for ajax call");
+        
+        var result = promise.defer();
+        o.success = function (data, textStatus, jqXHR) {
+            result.resolve([data, textStatus, jqXHR]);
+        };
+        
+        o.error = function (jqXHR, textStatus, errorThrow) {
+            result.reject([jqXHR, textStatus, errorThrow]);
+        };
+        
+        $.ajax(o);
+        return result.promise;
+    };
+    
+    return wrappedAjax;
+}
+
+module.exports = exports = wrapAjax;
